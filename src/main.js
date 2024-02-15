952643b1981addeb3d3f957612833d0e43af8bf8
rdzcn <ardaoezcan@gmail.com>
2024-02-15 15:34:14 +0300

    update styles

--- src/main.js
+++ src/main.js
@@ -4,13 +4,7 @@ import "./hyperdrive.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <button id="counter" type="button"></button>
   </div>
 `;
 
