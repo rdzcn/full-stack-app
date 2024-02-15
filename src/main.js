cfec2b7660d12c1c971b69b53194b1c13649ba7e
rdzcn <ardaoezcan@gmail.com>
2024-02-15 14:30:26 +0300

    working stars and canvas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import "./style.css";
 import { setupCounter } from "./counter.js";
+import Canvas from "./canvas.js";
+import Star from "./star.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -14,3 +16,44 @@ document.querySelector("#app").innerHTML = `
 `;
 
 setupCounter(document.querySelector("#counter"));
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Setup Objects
+  let canvas = new Canvas(document.querySelector("#canvas"));
+  canvas.translate(canvas.width / 2, canvas.height / 2);
+
+  let stars = [];
+  for (let i = 0; i < 1000; i++) {
+    stars.push(
+      new Star(
+        Math.random() * canvas.width - canvas.width / 2,
+        Math.random() * canvas.height - canvas.height / 2,
+        Math.random() * (canvas.width / 2),
+      ),
+    );
+  }
+
+  // Run logic
+  setInterval(function () {
+    for (let i = 0; i < stars.length; i++) {
+      stars[i].update();
+
+      if (stars[i].needsReset()) {
+        stars[i].reset(
+          Math.random() * canvas.width - canvas.width / 2,
+          Math.random() * canvas.height - canvas.height / 2,
+          Math.random() * (canvas.width / 2),
+        );
+      }
+    }
+  }, 1000 / 30);
+
+  // Draw Objects to the canvas
+  canvas.draw(
+    function () {
+      for (let i = 0; i < stars.length; i++) {
+        stars[i].draw(this);
+      }
+    }.bind(canvas),
+  );
+});
