583867ab3881fb6222ddc8c88de5a5483c1d74f9
rdzcn <ardaoezcan@gmail.com>
2024-02-20 09:42:22 +0300

    automata implementation

--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -5,3 +5,11 @@ export function getRandomHexColor() {
 export function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
+
+export function convertToBinary(number) {
+  let binary = number.toString(2);
+  while (binary.length < 8) {
+    binary = "0" + binary;
+  }
+  return binary;
+}
